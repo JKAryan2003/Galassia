e2da42008f261fdad363886c711e7c0fea549c46
JKAryan2003 <jkaryanrout5934@gmail.com>
2025-10-23 09:31:45 +0530

    add page transitions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -13,27 +13,44 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
-
+import { AnimatePresence, motion } from "framer-motion"
 
 const App = () => {
+  const location = useLocation()
+
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <ToastContainer />
       <Navbar />
       <SearchBar />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/product/:productId' element={<Product/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/place-order' element={<PlaceOrder/>}/>
-        <Route path='/orders' element={<Orders/>}/>
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path='/' element={<PageWrapper><Home/></PageWrapper>}/>
+          <Route path='/collection' element={<PageWrapper><Collection/></PageWrapper>}/>
+          <Route path='/about' element={<PageWrapper><About/></PageWrapper>}/>
+          <Route path='/contact' element={<PageWrapper><Contact/></PageWrapper>}/>
+          <Route path='/product/:productId' element={<PageWrapper><Product/></PageWrapper>}/>
+          <Route path='/cart' element={<PageWrapper><Cart/></PageWrapper>}/>
+          <Route path='/login' element={<PageWrapper><Login/></PageWrapper>}/>
+          <Route path='/place-order' element={<PageWrapper><PlaceOrder/></PageWrapper>}/>
+          <Route path='/orders' element={<PageWrapper><Orders/></PageWrapper>}/>
+        </Routes>
+      </AnimatePresence>
+    
       <Footer />
     </div>
+  )
+}
+
+const PageWrapper = ({ children }) => {
+  return (
+    <motion.div
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    exit={{ opacity: 0, y: -20 }}
+    transition={{ duration: 0.3 }}>
+      {children}
+    </motion.div>
   )
 }
 
